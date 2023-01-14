78a0ca4c70dd7cbcf13b5249df48e8a100d5f5d2
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-01-14 09:53:58 +0700

    Fix duplicate not count already added course and add new function to insert and delete

--- SQL/Registration.sql
+++ SQL/Registration.sql
@@ -10,6 +10,65 @@ create table CourseRegistration
     Course UNIQUEIDENTIFIER foreign key REFERENCES [dbo].[course](id),
     RegistrationStatus TINYINT
 )
+select * from CourseRegistration
+-- available course table
+    -- 0. Available
+    -- 1. Closed 
+create table AvailableCourse 
+(
+    Course UNIQUEIDENTIFIER foreign key REFERENCES [dbo].[course](id),
+    CourseStatus TINYINT
+)
+go
+
+select * from [dbo].[AvailableCourse]
+go
+-- procedures
+    -- add available course for registration
+    -- call this procedure whenever course creation
+create procedure AddAvailableCourse @CourseId UNIQUEIDENTIFIER
+as 
+    begin
+        insert into [dbo].[AvailableCourse] (Course,CourseStatus) values
+        (
+            @courseId,
+            0
+        )
+    end
+go
+    -- remove available course from registration
+create procedure LockAvailableCourse @CourseId UNIQUEIDENTIFIER
+as 
+    BEGIN 
+        update [dbo].[AvailableCourse]
+        SET CourseStatus = 1
+        WHERE Course = @CourseId
+    END
+go
+    -- remove ALL available course
+    -- call this procedure when finish course registering
+create procedure LockAllCurrentAvailableCourse
+as BEGIN
+    update [dbo].[AvailableCourse]
+    SET CourseStatus = 1
+END
+go
+    -- get all available course
+create procedure GetAvailableCourses
+as
+    begin
+        select [id],[coursename],[lecture_id],[course_code],[begin_date],[end_date],[session],[date_of_week] from [dbo].[AvailableCourse]
+            join [dbo].[course]
+            on [AvailableCourse].[Course] = [Course].[id]
+            where CourseStatus = 0
+    END
+go
+
+-- test executing
+EXEC AddAvailableCourse @CourseId ='60e63f23-a73c-41ac-b669-acda565413dd'
+EXEC LockAllCurrentAvailableCourse
+EXEC GetAvailableCourses
+
 -- add sample course with session and date
 insert into [dbo].[course] ([id],[coursename],[course_code],[begin_date],[end_date],[session],[date_of_week]) VALUES 
 (
@@ -59,6 +118,140 @@ select * from course
 
 -- check registration valid with json
 
+
+
+
+
+
+
+declare @Json NVARCHAR(max);
+set @Json = N'
+{
+    "student":"c2c3f5ad-03aa-4691-b779-268454132de5",
+    "courses":[
+          "60e63f23-a73c-41ac-b669-acda565413dd"
+    ]
+}'
+EXEC FindDuplicatedSchedule @Json=@Json
+
+select student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week  from (select courseId,student from (select * from openjson(@Json) with (
+        student UNIQUEIDENTIFIER '$.student',
+        courses NVARCHAR(max) '$.courses' as json
+        ) j1
+        outer apply openjson(j1.courses) with (
+            courseId UNIQUEIDENTIFIER '$'
+        )  
+        ) as JsonResult
+        union 
+        (
+            select Course as courseId, Student as student from CourseRegistration
+            where Student = JSON_VALUE(@Json,'$.student')
+        )) as j2
+    join course on j2.courseId = course.id  
+
+
+
+select distinct * from CourseRegistration
+
+
+GO
+-- procedure for dinding any duplicated course in a JSON request containing student registration information
+create PROCEDURE FindDuplicatedSchedule @Json NVARCHAR(max)
+as 
+begin
+    select t1.student,t1.courseId,t1.begin_date,t1.end_date,t1.[session],t1.[date_of_week] from (
+        select student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week  from (select courseId,student from (select * from openjson(@Json) with (
+        student UNIQUEIDENTIFIER '$.student',
+        courses NVARCHAR(max) '$.courses' as json
+        ) j1
+        outer apply openjson(j1.courses) with (
+            courseId UNIQUEIDENTIFIER '$'
+        )  
+        ) as JsonResult
+        union 
+        (
+            select Course as courseId, Student as student from CourseRegistration
+            where Student = JSON_VALUE(@Json,'$.student')
+        )) as j2
+    join course on j2.courseId = course.id) 
+    as t1
+    join 
+    (
+        select [session],[date_of_week], count(*) as qty from 
+            (select student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week  from (select courseId,student from (select * from openjson(@Json) with (
+            student UNIQUEIDENTIFIER '$.student',
+            courses NVARCHAR(max) '$.courses' as json
+            ) j1
+            outer apply openjson(j1.courses) with (
+                courseId UNIQUEIDENTIFIER '$'
+            )  
+            ) as JsonResult
+            union 
+            (
+                select Course as courseId, Student as student from CourseRegistration
+                where Student = JSON_VALUE(@Json,'$.student')
+            )) as j2
+            join course on j2.courseId = course.id  
+            ) as t3
+            group by t3.[session],t3.[date_of_week]
+            having count(*) > 1
+    ) as t2
+    on t1.[session] = t2.[session] and t1.[date_of_week]=t2.[date_of_week]
+END
+GO
+
+drop procedure FindDuplicatedSchedule
+go
+
+-- procedure for register 
+create procedure RegisterCourse @Json NVARCHAR(max)
+AS
+    BEGIN
+        insert into [dbo].[CourseRegistration]
+        select distinct [student] as Student,[courseId] as Course, 0 as [RegistrationStatus] from openjson(@Json) with 
+                (
+                student UNIQUEIDENTIFIER '$.student',
+                courses NVARCHAR(max) '$.courses' as json
+                ) j1
+                outer apply openjson(j1.courses) with (
+                    courseId UNIQUEIDENTIFIER '$'
+                )
+
+    END
+go
+
+-- procedure for delete registration using json
+create procedure DeleteRegistration @Json NVARCHAR(max)
+as
+    BEGIN
+        delete from [dbo].[CourseRegistration] where 
+        Student = JSON_VALUE(@Json,'$.student')
+        and Course in (
+            select Course from openjson(@Json,'$.courses') with (
+            Course UNIQUEIDENTIFIER '$'
+        )
+    )
+    END
+go
+-- procedure for transfer all registration into course_user
+
+
+
+
+-- test Register
+declare @test NVARCHAR(max);
+set @test = N'
+{
+    "student":"c2c3f5ad-03aa-4691-b779-268454132de5",
+    "courses":[
+        "c9333905-6ab8-4318-bea5-7bfe00f070ee"
+    ]
+}'
+Exec RegisterCourse @Json = @test
+
+select * from CourseRegistration
+
+-- test delete Registration
 declare @test NVARCHAR(max);
 set @test = N'
 {
@@ -69,42 +262,7 @@ set @test = N'
         "60e63f23-a73c-41ac-b669-acda565413dd"
     ]
 }'
-EXEC FindDuplicatedSchedule @json=@test
-
-
-GO
-
-create PROCEDURE FindDuplicatedSchedule @json NVARCHAR(max)
-as 
-begin
-    select t1.student,t1.courseId,t1.begin_date,t1.end_date,t1.[session],t1.[date_of_week] from (select j1.student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week from openjson(@json) with (
-        student UNIQUEIDENTIFIER '$.student',
-        courses NVARCHAR(max) '$.courses' as json
-    ) j1
-    outer apply openjson(j1.courses) with (
-        courseId UNIQUEIDENTIFIER '$'
-    ) j2
-    join course on j2.courseId = course.id) as t1
-    join 
-    (
-        select [session],[date_of_week], count(*) as qty from 
-            (select j1.student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week from openjson(@json) with (
-            student UNIQUEIDENTIFIER '$.student',
-            courses NVARCHAR(max) '$.courses' as json
-            ) j1
-            outer apply openjson(j1.courses) with (
-                courseId UNIQUEIDENTIFIER '$'
-            ) j2
-            join course on j2.courseId = course.id) as t3
-            group by t3.[session],t3.[date_of_week]
-            having count(*) > 1
-    ) as t2
-    on t1.[session] = t2.[session] and t1.[date_of_week]=t2.[date_of_week]
-END
-GO
-
-
-
+EXEC DeleteRegistration @Json=@test
 
 
 
