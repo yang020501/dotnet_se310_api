4926ddbe5e5df1c79f4a60211f9e999f72951c84
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-01-14 10:01:37 +0700

    Refactor SQL document

--- SQL/Registration.sql
+++ SQL/Registration.sql
@@ -26,48 +26,9 @@ go
 -- procedures
     -- add available course for registration
     -- call this procedure whenever course creation
-create procedure AddAvailableCourse @CourseId UNIQUEIDENTIFIER
-as 
-    begin
-        insert into [dbo].[AvailableCourse] (Course,CourseStatus) values
-        (
-            @courseId,
-            0
-        )
-    end
-go
-    -- remove available course from registration
-create procedure LockAvailableCourse @CourseId UNIQUEIDENTIFIER
-as 
-    BEGIN 
-        update [dbo].[AvailableCourse]
-        SET CourseStatus = 1
-        WHERE Course = @CourseId
-    END
-go
-    -- remove ALL available course
-    -- call this procedure when finish course registering
-create procedure LockAllCurrentAvailableCourse
-as BEGIN
-    update [dbo].[AvailableCourse]
-    SET CourseStatus = 1
-END
-go
-    -- get all available course
-create procedure GetAvailableCourses
-as
-    begin
-        select [id],[coursename],[lecture_id],[course_code],[begin_date],[end_date],[session],[date_of_week] from [dbo].[AvailableCourse]
-            join [dbo].[course]
-            on [AvailableCourse].[Course] = [Course].[id]
-            where CourseStatus = 0
-    END
-go
 
--- test executing
-EXEC AddAvailableCourse @CourseId ='60e63f23-a73c-41ac-b669-acda565413dd'
-EXEC LockAllCurrentAvailableCourse
-EXEC GetAvailableCourses
+
+
 
 -- add sample course with session and date
 insert into [dbo].[course] ([id],[coursename],[course_code],[begin_date],[end_date],[session],[date_of_week]) VALUES 
@@ -115,46 +76,8 @@ delete from [dbo].[course] where id = '9594515d-69e4-4252-a323-d5166a6bbae9'
 
 -- select course
 select * from course
+go
 
--- check registration valid with json
-
-
-
-
-
-
-
-declare @Json NVARCHAR(max);
-set @Json = N'
-{
-    "student":"c2c3f5ad-03aa-4691-b779-268454132de5",
-    "courses":[
-          "60e63f23-a73c-41ac-b669-acda565413dd"
-    ]
-}'
-EXEC FindDuplicatedSchedule @Json=@Json
-
-select student,j2.courseId,begin_date,course.end_date,course.[session],course.date_of_week  from (select courseId,student from (select * from openjson(@Json) with (
-        student UNIQUEIDENTIFIER '$.student',
-        courses NVARCHAR(max) '$.courses' as json
-        ) j1
-        outer apply openjson(j1.courses) with (
-            courseId UNIQUEIDENTIFIER '$'
-        )  
-        ) as JsonResult
-        union 
-        (
-            select Course as courseId, Student as student from CourseRegistration
-            where Student = JSON_VALUE(@Json,'$.student')
-        )) as j2
-    join course on j2.courseId = course.id  
-
-
-
-select distinct * from CourseRegistration
-
-
-GO
 -- procedure for dinding any duplicated course in a JSON request containing student registration information
 create PROCEDURE FindDuplicatedSchedule @Json NVARCHAR(max)
 as 
@@ -219,6 +142,8 @@ AS
 
     END
 go
+drop procedure RegisterCourse
+go
 
 -- procedure for delete registration using json
 create procedure DeleteRegistration @Json NVARCHAR(max)
@@ -233,10 +158,64 @@ as
     )
     END
 go
+drop procedure DeleteRegistration
+go
+
+-- Procedure for add a course to register, this procedure is called when a new course is created
+create procedure AddAvailableCourse @CourseId UNIQUEIDENTIFIER
+as 
+    begin
+        insert into [dbo].[AvailableCourse] (Course,CourseStatus) values
+        (
+            @courseId,
+            0
+        )
+    end
+go
+
+-- Procedure for Lock Particular course (make it not visible to Register)
+create procedure LockAvailableCourse @CourseId UNIQUEIDENTIFIER
+as 
+    BEGIN 
+        update [dbo].[AvailableCourse]
+        SET CourseStatus = 1
+        WHERE Course = @CourseId
+    END
+go
+
+-- Procedure for lock all available course, This procedure is called when finish course registering
+create procedure LockAllCurrentAvailableCourse
+as BEGIN
+    update [dbo].[AvailableCourse]
+    SET CourseStatus = 1
+END
+go
+
+-- Procedure to get available course for registration
+create procedure GetAvailableCourses
+as
+    begin
+        select [id],[coursename],[lecture_id],[course_code],[begin_date],[end_date],[session],[date_of_week] from [dbo].[AvailableCourse]
+            join [dbo].[course]
+            on [AvailableCourse].[Course] = [Course].[id]
+            where CourseStatus = 0
+    END
+go
+
+
 -- procedure for transfer all registration into course_user
+-- TODO: create a procedure to transfer all course registration result into course_user
 
-
-
+-- test check duplicated 
+declare @Json NVARCHAR(max);
+set @Json = N'
+{
+    "student":"c2c3f5ad-03aa-4691-b779-268454132de5",
+    "courses":[
+          "60e63f23-a73c-41ac-b669-acda565413dd"
+    ]
+}'
+EXEC FindDuplicatedSchedule @Json=@Json
 
 -- test Register
 declare @test NVARCHAR(max);
@@ -264,7 +243,14 @@ set @test = N'
 }'
 EXEC DeleteRegistration @Json=@test
 
+-- Test add available course
+EXEC AddAvailableCourse @CourseId ='60e63f23-a73c-41ac-b669-acda565413dd'
 
+-- Test lock all course
+EXEC LockAllCurrentAvailableCourse
+
+-- Test get available course for registration
+EXEC GetAvailableCourses
 
 
 
